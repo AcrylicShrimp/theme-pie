acec9ea23325af464cfbde4f5762d9e6395dd450
AcrylicShrimp <led789zxpp@naver.com>
2021-03-31 11:58:04 +0900

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -13,14 +13,14 @@ function fish_prompt
         echo -n -s $branch_color (git_branch_name) $normal_color
         set -l git_meta ""
         if test (command git ls-files --others --exclude-standard | wc -w 2> /dev/null) -gt 0
-            set git_meta "$git_meta"
+            set git_meta "$git_meta?"
         end
         if test (command git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
-            set git_meta "$git_meta"
+            set git_meta "$git_meta!"
         end
         if git_is_touched
-            git_is_dirty && set git_meta "$git_meta"
-            git_is_staged && set git_meta "$git_meta"
+            git_is_dirty && set git_meta "$git_metaD"
+            git_is_staged && set git_meta "$git_metaS"
         end
         set -l commit_count (command git rev-list --count --left-right (git remote)/(git_branch_name)"...HEAD" 2> /dev/null)
         if test $commit_count
